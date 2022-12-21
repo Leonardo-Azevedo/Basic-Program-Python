d59b760f9ec29e8c52d1a05e541e0138916af3d6
Leonardo Azevedo <leonardo.morcego@hotmail.com>
2022-12-20 21:01:41 -0300

    correção main

--- main.py
+++ main.py
@@ -2,9 +2,4 @@ from funções import principais
 from funções.arquivo import *
 
 #funções.Menu()
-#principais.verify()
-
-lista = [2, 3, 5]
-
-for n1,n2 in enumerate(lista):
-    print(n1+n2)
+principais.verify()
